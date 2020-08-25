71bd98e524f529e1f2d9468df97de1f281246fc8
chenraff <chenraff96@gmail.com>
2020-08-25 08:20:47 +0000

    Implement query of optional time slots for given meeting

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,69 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+
 
 public final class FindMeetingQuery {
-  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
-  }
+
+    /**
+    * Returns a Collection of optional TimeRange slots for the requested meeting 
+    * (when all the meeting attendees are free)
+    */
+    public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
+        // Blocked time slots
+        List<TimeRange> badTimeRanges = getBadTimeRanges(events, request);
+        badTimeRanges.sort(TimeRange.ORDER_BY_START);
+        return getPossibleTimeRanges(badTimeRanges, request);
+    }
+
+    /**
+    * Returns a List of Blocked TimeRange slots for the given meeting request
+    * (at least one of the meeting attendees go to an event from the given events
+    * collection in this time slot) 
+    */
+    public List<TimeRange> getBadTimeRanges(Collection<Event> events, MeetingRequest request) {
+        List<TimeRange> badTimeRanges = new ArrayList<>();
+        Collection<String> meetingAttendees =  request.getAttendees();
+        
+        for (Event event: events) {
+            Set<String> intersection = new HashSet<>(event.getAttendees());
+            intersection.retainAll(meetingAttendees);
+            // At least one of the meeting attendees go to this event
+            if (!intersection.isEmpty()) {
+                badTimeRanges.add(event.getWhen());
+            }
+        }
+        return badTimeRanges;
+    }
+
+    /**
+    * Returns a Collection of optional TimeRange slots for the requested meeting 
+    * between the given bad TimeRange slots
+    */
+    public Collection<TimeRange> getPossibleTimeRanges(List<TimeRange> badTimeRanges, MeetingRequest request) {
+        Collection<TimeRange> possibleTimeRanges = new ArrayList<>();
+        int currStart=TimeRange.START_OF_DAY;
+        int currEnd;
+        for (TimeRange time: badTimeRanges) {
+            currEnd = time.start();
+            // Current slot is optional for the request meeting
+            if (currEnd-currStart >= request.getDuration()) {
+                possibleTimeRanges.add(TimeRange.fromStartEnd(currStart, currEnd, false));
+            }
+            // Promotes currStart to next free slot beginning or
+            // stays put if previous badTimeSlot contains the current
+            currStart = Math.max(currStart, time.end());
+        }
+        // Check slot until end of the day
+        if (TimeRange.END_OF_DAY - currStart >= request.getDuration()) {
+            possibleTimeRanges.add(TimeRange.fromStartEnd(currStart, TimeRange.END_OF_DAY, true));
+        }
+        return possibleTimeRanges;
+    }
 }
